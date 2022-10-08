ebc014521872bc4950ee70f4b9479279d8f35bba
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2022-10-08 19:44:51 +0300

    Generic structures append.

---
+++ Data Structures/Apps/Program.cs
@@ -0,0 +1,115 @@
+﻿
+
+
+
+using System.Collections;
+using System.Linq;
+
+namespace Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var p1 = new DataStructures.Array.Array<int>(1,2,3,4);
+            //var p2 = new int[] { 5, 6, 7, 8 };
+            //var p3 = new List<int>() { 9, 10, 11, 12 };
+            //var p4 = new ArrayList() { 13, 14, 15, 16 }; // ArrayList'i parametre olarak döngüye veremeyiz çünkü IEnumerable yapısı tip güvenliği sağlıyor.
+
+
+            //var arr = new DataStructures.Array.Array<int>();
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    arr.add(i + 1);
+            //    Console.WriteLine($"{i+1} has been added to array.");
+            //    Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+            //}
+
+            //Console.WriteLine(); 
+
+            //for (int i = arr.Count; i >= 1; i--)
+            //{
+            //    Console.WriteLine($"{arr.remove()} has been removed from the array.");
+            //    Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+            //}
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            var arr = new DataStructures.Array.Array<int>(1,3,5,7);
+            // var crr = (DataStructures.Array.Array<int>)arr.Clone();
+            var crr = arr.Clone() as DataStructures.Array.Array<int>;
+
+            arr.add(89); // Klonlanmış nesne ile ilk nesne arasında referans ilişkisi olmadığı için değişiklikler o nesne ile sınırlı kalacaktır.
+            // deep copy'de nesne sıfırdan oluşturulur ancak yine referans yoktur.
+            foreach (var item in arr)
+            {
+                Console.Write($"{item, -3}");
+            }
+            Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+
+            Console.WriteLine();           
+
+            foreach (var item in crr)
+            {
+                Console.Write($"{item,-3}");
+            }
+            Console.WriteLine($"{crr.Count}/{crr.Capacity}");
+
+            Console.ReadKey();
+        }
+
+        /* void diziIslemleri()
+        {
+            arr.add(1);
+            arr.add(2);
+            arr.add(3);
+            arr.add(4);
+            arr.add(5);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------");
+            arr.Where(x => x % 2 == 0).ToList().ForEach(x => Console.WriteLine(x)); // ikiye bölünebilen elemanları filtreler.
+            Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+        }
+        */
+
+        void Bilgiler()
+        {
+            // Array 
+            var arrayChar = new char[] { 'y', 'r', 'g' }; // Bu dizi sabit boyutludur.
+            var arrayInt = Array.CreateInstance(typeof(int), 5);
+            arrayInt.SetValue(8, 0); // arrayInt[0] = 8 değerini atar.
+            arrayInt.GetValue(0); // 0. indexteki elemanı döndürür. Yani 8 
+            Console.WriteLine(arrayInt.Length);
+
+
+            // ArrayList 
+            // Dizi üzerinde yönetimsel fonksiyonlara ihtiyaç duyulduğunda Array yerine ArrayList kullanılmalıdır.
+            // type-safe yoktur.
+            // 5 -> boxing -> object
+            // a -> boxing -> object
+            // object -> unboxing - int
+            var arrayObj = new ArrayList();
+            arrayObj.Add(5);
+            arrayObj.Add('a');
+            Console.WriteLine(arrayObj.Count);
+
+            // List<T> 
+            // Genelde işlem yapılan veri yapıları koleksiyon türüne indirgenerek tanımlanır.
+            var arrInt = new List<int>();
+            arrInt.Add(10);
+            arrInt.AddRange(new int[] { 1, 2, 3 });
+            arrInt.RemoveAt(0);
+            Console.WriteLine(arrInt.Count);
+            foreach (var item in arrInt)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Data Structures/DataStructures/Array/Array.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Array
+{
+    public class Array<T> :  IEnumerable<T>,ICloneable // <T> şeklinde tanımlamakla birlikte tip güvenliğini sağlıyoruz ve boxing-unboxing işlemlerine gerek kalmıyor.
+    {
+        // IEnumerable yapısı GetEnumerator'ı garanti ettiği için foreach döngüsünü rahatlıkla kullanabiliyoruz.
+        // IEnumerable<T> ile koleksiyonu numaralandırılabilir bir yeteneğe kavuşturmuş oluyoruz.
+        // Aynı zamanda dile entegre sorguları da çalıştırabiliyoruz. ( LINQ = Language INtegrated Query)
+        // IEnumerable<T> ifadesi ile koleksiyondan her defasında bir örnek seçip kullanabilme imkanı sağlamaktadır. Yani kısaca numaralandırma işlemi yapmamızı sağlamaktadır.
+        private T[] InnerList;
+
+        public int Count { get; private set; }
+        public int Capacity => InnerList.Length;
+
+        public Array()
+        {
+            InnerList = new T[2];
+            Count = 0;
+        }
+
+        public Array(params T[] initial)
+        {
+            InnerList = new T[initial.Length];
+            Count = 0;
+            foreach(var item in initial)
+            {
+                add(item); 
+            }
+        }
+
+        public Array(IEnumerable<T> collection)
+        {
+            InnerList = new T[collection.ToArray().Length];
+            Count = 0;
+            foreach(var item in collection)
+            {
+                add(item);
+            }
+        }
+
+        public void add(T item)
+        {
+            if (InnerList.Length == Count)
+            {
+                doubleArray();
+            }
+            InnerList[Count] = item;
+            Count++;
+        }
+
+        private void doubleArray()
+        {
+            var temp = new T[InnerList.Length * 2];
+            /*
+            for (int i = 0; i < InnerList.Length; i++)
+            {
+                temp[i] = InnerList[i];
+            }
+            */
+            System.Array.Copy(InnerList, temp, InnerList.Length);
+            InnerList = temp;
+        }
+
+        public T remove()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("There is no more item to be removed from the array.");
+            }
+
+            if (InnerList.Length / 4 == Count)
+            {
+                halfArray();
+            }
+            var temp = InnerList[Count - 1];
+            if (Count > 0)
+            {
+                Count--;
+            }
+            return temp;
+        }
+
+        private void halfArray()
+        {
+            if (InnerList.Length > 2)
+            {
+                var temp = new T[InnerList.Length / 2];
+                System.Array.Copy(InnerList,temp,InnerList.Length/4);
+                InnerList = temp;
+            }
+        }
+
+        public object Clone()
+        {
+            //return this.MemberwiseClone(); // Sığ copy 
+            
+            // deep copy
+            var arr = new Array<T>();
+            foreach (var item in this)
+            {
+                arr.add(item);
+            }
+            return arr;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InnerList.Select(x => x).GetEnumerator();
+            // return InnerList.Take(Count).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
